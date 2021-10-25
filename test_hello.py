101fd3266e0a3a587ee0e86688bb02b4dfad7a18
vradicevic <47304793+vradicevic@users.noreply.github.com>
2021-10-25 09:24:38 +0200

    Fix Bob to Thor assert in test

--- test_hello.py
+++ test_hello.py
@@ -4,4 +4,4 @@ from hello import hello
 def test_hello():
     runner = CliRunner()
     result = runner.invoke(hello,["--name","Thor", "--color", "blue"])
-    assert "Bob" in result.output
+    assert "Thor" in result.output
