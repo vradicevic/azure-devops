39102e547620a5e2838219a3845885ef8d466526
Valentin <vradicevic@RTRKT060.domain.local>
2021-10-25 09:11:24 +0200

    Add Make step lint

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
 import click
-
+var = 
 
 @click.command(help="This is hello app")
 @click.option("--name",prompt="I need name",help="Need name")
 @click.option("--color",prompt="I need color",help="Need color")
 
 def hello(name,color):
-        if name == "Thor":
-            click.echo("Thor you r always red")
+    if name == "Thor":
+        click.echo("Thor you r always red")
             
-        else:
-            click.echo(click.style(f"Hello {name}",fg=color))
+    else:
+        click.echo(click.style(f"Hello {name}",fg=color))
 
 if __name__ == "__main__":
     hello()
